e2f081d8d0d1ca7627fd9ce96ea8262203a9aca0
vikram <vikramkunwar1404@gmail.com>
2023-05-30 14:31:54 +0530

    Image Picker [Release 1.0.0]

--- ImageGallery/ViewController.swift
+++ ImageGallery/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var galleryImage : UIImageView!
+    
+    @IBOutlet weak var galleryBtn : UIButton!
+    
+    @IBOutlet weak var imageView : UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        galleryImage.layer.cornerRadius = 15
+        
+        imageView.layer.cornerRadius = 15
+        galleryBtn.layer.cornerRadius = 15
+    }
+    
+    @IBAction func galleryOpenBtn(_ sender: UIButton){
+        
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.delegate = self
+            imagePickerController.sourceType = .photoLibrary
+            present(imagePickerController, animated: true, completion: nil)
+        
+        
     }
 
 
+}
+extension ViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                // Set the selected image to the UIImageView
+                galleryImage.image = selectedImage
+            }
+            
+            picker.dismiss(animated: true, completion: nil)
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            picker.dismiss(animated: true, completion: nil)
+        }
 }
 
